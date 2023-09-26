19570bf16a8a9bd336cf015e0d332fb5a2ca4f8f
nasrin jafari <nasrin.jafari770@gmail.com>
2023-09-26 20:07:05 +0330

    desing exercise card

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Stack, Typography } from "@mui/material";
-
 const ExerciseCard = ({ exercise }) => (
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
