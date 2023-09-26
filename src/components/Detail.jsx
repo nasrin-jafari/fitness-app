1ba15b71f913bcfa7e0e870143d116ebe50c7b55
nasrin jafari <nasrin.jafari770@gmail.com>
2023-09-26 20:07:28 +0330

    design detail exercise

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Typography, Stack, Button } from "@mui/material";
-
 import BodyPartImage from "../assets/icons/body-part.png";
 import TargetImage from "../assets/icons/target.png";
 import EquipmentImage from "../assets/icons/equipment.png";
-
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
