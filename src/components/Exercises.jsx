607f9ce19945d8f8d60a26486bba3802f6553eff
nasrin jafari <nasrin.jafari770@gmail.com>
2023-09-26 20:05:49 +0330

    desing exercises

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
-
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from "./ExerciseCard";
 import Loader from "./Loader";
-
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [exercisesPerPage] = useState(6);
@@ -39,7 +37,6 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     indexOfFirstExercise,
     indexOfLastExercise
   );
-
   const paginate = (event, value) => {
     setCurrentPage(value);
 
