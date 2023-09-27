1c8bc4674900e405b0b984ef8323bda20f3f35eb
nasrin jafari <nasrin.jafari770@gmail.com>
2023-09-27 10:11:06 +0330

    change api key

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 import { Button, Stack, Typography } from "@mui/material";
 const ExerciseCard = ({ exercise }) => (
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
-    <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    <img
+      src={exercise.gifUrl}
+      alt={exercise.name}
+      loading="lazy"
+    />
+
     <Stack direction="row">
       <Button
         sx={{
